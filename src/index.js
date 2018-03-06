6250a9b6cf5d7da6451003b94a5b749f474ff306
Alexander Vaganov <ar.vaganov@gmail.com>
2018-03-06 15:25:42 +0300

    Revert "configure project for migration to React"
    This reverts commit c8d6000986badb35b26682d7e3ac9d9acca857c2.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import AppContainer from './containers/AppContainer';
-
-render(
-  <AppContainer />,
-  document.getElementById('app'),
-);
